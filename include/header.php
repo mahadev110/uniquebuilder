d08707c48d5fea020b626e6d893e56c909f97bb4
majestic <magadev110@gmail.com>
2023-07-18 13:20:43 +0530

    meta

--- include/header.php
+++ include/header.php
@@ -5,8 +5,8 @@
     <meta charset="utf-8">
     <title>Unique Builder</title>
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
-    <meta content="Free HTML Templates" name="keywords">
-    <meta content="Free HTML Templates" name="description">
+    <meta content="unique builder" name="keywords">
+    <meta content="unique builder" name="description">
 
     <!-- Favicon -->
     <link href="img/logo.png" rel="icon">
